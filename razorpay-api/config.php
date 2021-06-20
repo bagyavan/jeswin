fd08abc7a09eeaac009e981e09f6ecf471034a5f
Jeswin Antony <50578578+bagyavan@users.noreply.github.com>
2021-06-20 14:29:11 +0530

    razor

---
+++ razorpay-api/config.php
@@ -0,0 +1,17 @@
+<?php
+
+$keyId = 'rzp_test_NQqAADY2gKCkm3';
+$keySecret = '5karYKttDTfjUThJkQVoiEKd';
+$displayCurrency = 'INR';
+
+//These should be commented out in production
+// This is for error reporting
+// Add it to config.php to report any errors
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
+// Database Connection Details
+$host = "localhost";
+$username = "root";
+$password = "";
+$dbname = "das";
---
+++ razorpay-api/verify.php
@@ -0,0 +1,62 @@
+<?php
+
+require('config.php');
+
+$con = mysqli_connect($host,$username,$password,$dbname);
+date_default_timezone_set('Asia/Kolkata');
+$today = date("d-m-Y");
+
+session_start();
+
+require('razorpay-php/Razorpay.php');
+use Razorpay\Api\Api;
+use Razorpay\Api\Errors\SignatureVerificationError;
+
+$success = true;
+
+$error = "Payment Failed";
+
+if (empty($_POST['razorpay_payment_id']) === false)
+{
+    $api = new Api($keyId, $keySecret);
+
+    try
+    {
+        // Please note that the razorpay order ID must
+        // come from a trusted source (session here, but
+        // could be database or something else)
+        $attributes = array(
+            'razorpay_order_id' => $_SESSION['razorpay_order_id'],
+            'razorpay_payment_id' => $_POST['razorpay_payment_id'],
+            'razorpay_signature' => $_POST['razorpay_signature']
+        );
+
+        $api->utility->verifyPaymentSignature($attributes);
+    }
+    catch(SignatureVerificationError $e)
+    {
+        $success = false;
+        $error = 'Razorpay Error : ' . $e->getMessage();
+    }
+}
+
+if ($success === true)
+{
+
+    $razorpay_order_id = $_SESSION['razorpay_order_id'];
+    $razorpay_payment_id = $_POST['razorpay_payment_id'];
+    $email = $_SESSION['email'];
+
+    $sql = "INSERT INTO `tbl_transactions` (`order_id`, `payment_id`, `status`, `email`, `dates`) VALUES ('$razorpay_order_id', '$razorpay_payment_id', 'Success', '$email','$today')";
+    mysqli_query($con,$sql);
+
+    $html = "<p>Your payment was successful</p>
+             <p>Payment ID: {$_POST['razorpay_payment_id']}</p>";
+}
+else
+{
+    $html = "<p>Your payment failed</p>
+             <p>{$error}</p>";
+}
+
+echo $html;
